e7d88df8f548026104d04bd71e70b32f2028273e
dave ready <davis_ready@hotmail.com>
2020-12-20 19:33:06 -0500

    on click for searchBtn

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var cityName = $("#citySearch").val();
+var currentDate = moment().format(); 
 
 var apiKey = "42709480618f4f67c2c902c0781e5e4a";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+
+//var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
 
 //Run AJAX call to OpenWeatherMap API
 
@@ -13,6 +17,13 @@ $.ajax({
     console.log(response);
 });
 
+
+
+$("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+    var citySearch = $("#citySearch").val();
+
+})
 
 
 //var weather = ;
