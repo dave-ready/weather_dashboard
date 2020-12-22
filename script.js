15b108adb9f6ee68117321127b6895039486c98c
dave ready <davis_ready@hotmail.com>
2020-12-22 15:03:53 -0500

    searchBtn on click function debugged and completed + html cleanup/edits

--- script.js
+++ script.js
@@ -1,34 +1,63 @@
-//store city name
-var cityName = $("#citySearch").val();
 
-var currentDate = moment().format(); 
+
+var currentDate = moment().format("L"); 
 
 var apiKey = "7fd432db80c8966a57818fd7382af9b7";
-//var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-//var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+
+//var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=" + apiKey + "&q=" + cityName;
+
+
+$("#citySearch").keypress(function(event) {
+
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    $("#searchBtn").click();
+
+   }
+});
+
+//on click
+$("#searchBtn").on("click", function() {
+
+//get city name from user
+var cityName = $("#citySearch").val();
+
+//clears input field after search
+$("#citySearch").val("");
 
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=" + apiKey + "&q=" + cityName;
 
-
-// Create a function to search for city's weather
-
-function currentWeatherSearch() {
-
-//Convert temp from kelvin to fahrenheit
-var convertF = (response.main.temp - 273.15) * 1.8 + 32;
-
-//empty div to hold text content
-$("#currentCity").empty();
-
-//Run AJAX call to OpenWeatherMap API
-
+//ajax call
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
     console.log(queryURL);
     console.log(response);
+    console.log(response.name)
+
+    //Convert temp from kelvin to fahrenheit
+var convertF = (response.main.temp - 273.15) * 1.8 + 32;
+    console.log(convertF)
+    console.log(response.main.humidity)
+    console.log(response.wind.speed)
+
+    //currentWeatherSearch();
+    //fiveDayForecastSearch();
+    //searchHistoryList();
+    
+    });
+
 });
+
+// Create a function to search for city's weather
+
+//function currentWeatherSearch(cityName) {
+
+//Run AJAX call to OpenWeatherMap API
+
+
+
 
 // create HTML elements for weather data
 
@@ -42,12 +71,6 @@ var currentWeather = response.weather[0].main;
 var windSpeedEl = $("<p>").addClass("card-text current-wind").text("Wind Speed:" + " " + response.wind.speed + " " + "MPH");
 var humidityEl = $("<p>").addClass("card-text current-humidity").text("Humidity:" + " " + response.main.humidity + "%");
 
-//Append Elements to page
-
-cityNameEl.append(dateEl, weatherIMG);
-cardBodyEl.append(cityNameEl, tempEl, humidityEl, windSpeedEl);
-cardEl.append(cardBodyEl);
-$("#currentWeather").append(cardEl)
 
 // add weather icon for the appropriate weather -- If/Else statement??
 
@@ -72,28 +95,43 @@ if (currentWeather === "rain") {
         weatherIMG.attr("style", "height: 50px; width: 50px");
 }
 
-}
+//empty div to hold text content
+var newDiv = $('<div>');
+
+newDiv.append(displayMainDate, currentIcon, tempEL, humEl, windEl);
+
+$("#currentCity").html(newDiv);
+
+
+//Append Elements to page
+cityNameEl.append(dateEl, weatherIMG);
+cardBodyEl.append(cityNameEl, tempEl, humidityEl, windSpeedEl);
+cardEl.append(cardBodyEl);
+$("#currentWeather").append(cardEl)
+
+
 //currentWeatherSearch()
 
-//on click
-$("#searchBtn").on("click", function(event) {
 
-    //Prevent default
-    event.preventDefault();
+//save to local storage
+var storageArray = [];
+var storedText = $(this).siblings("input").val();
 
-    //save to local storage
-    var storageArray = [];
-    var storedText = $(this).siblings("input").val();
-
-    storageArray.push(storedText);
-    localStorage.setItem("cityName", JSON.stringify(storageArray));
-
-})
+storageArray.push(storedText);
+localStorage.setItem("cityName", JSON.stringify(storageArray));
 
 
 
 // append HTML search history
+
+function searchHistoryList() {
+    var searchItem = $("<li>").addClass("list-group-item").text(cityName);
+}
+//searchHistoryList();
+
+
 //"on click" for search history buttons
+//five day forecast function
 
 
 
